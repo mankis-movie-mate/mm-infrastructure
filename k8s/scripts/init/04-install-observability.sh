0a98f9570c6cf2c0776a858b5bd0537923a9f5f2
ulcheyev <ulcheyev@fel.cvut.cz>
2025-08-28 15:39:48 +0200

    [Mergings main and master]

--- k8s/scripts/init/04-install-observability.sh
+++ k8s/scripts/init/04-install-observability.sh
@@ -11,4 +11,8 @@ helm upgrade --install mm-grafana     grafana/grafana                           
 helm upgrade --install mm-otel-collector open-telemetry/opentelemetry-collector -f "$OBSERVABILITY_DIR/mm-otel/values.local.yaml"    -n "$NAMESPACE"
 helm upgrade --install mm-loki        grafana/loki                              -f "$OBSERVABILITY_DIR/mm-loki/values.yaml"          -n "$NAMESPACE"
 helm upgrade --install mm-alloy       grafana/alloy                             -f "$OBSERVABILITY_DIR/mm-alloy/values.yaml"         -n "$NAMESPACE"
+<<<<<<< HEAD
 helm upgrade --install mm-openapi-hub       ../../../../mm-openapi-hub/helm      -f ../../../../mm-openapi-hub/helm/values.local.yaml        -n "$NAMESPACE"
+=======
+helm upgrade --install mm-openapi-hub       ../../../../mm-openapi-hub/helm/mm-openapi-hub      -f ../../../../mm-openapi-hub/helm/mm-openapi-hub/values.local.yaml        -n "$NAMESPACE"
+>>>>>>> master
--- k8s/scripts/init/05-install-core.sh
+++ k8s/scripts/init/05-install-core.sh
@@ -8,3 +8,7 @@ CORE_DIR="../../core"
 helm upgrade --install mm-user-service     "$CORE_DIR/mm-user-service"     -f "$CORE_DIR/mm-user-service/values.local.yaml"     -n "$NAMESPACE"
 helm upgrade --install mm-api-gateway      "$CORE_DIR/mm-api-gateway"      -f "$CORE_DIR/mm-api-gateway/values.local.yaml"      -n "$NAMESPACE"
 helm upgrade --install mm-discovery-server "$CORE_DIR/mm-discovery-server" -f "$CORE_DIR/mm-discovery-server/values.local.yaml" -n "$NAMESPACE"
+<<<<<<< HEAD
+=======
+helm upgrade --install mm-recommendation-service "$CORE_DIR/mm-recommendation-service" -f "$CORE_DIR/mm-recommendation-service/values.local.yaml" -n "$NAMESPACE"
+>>>>>>> master
